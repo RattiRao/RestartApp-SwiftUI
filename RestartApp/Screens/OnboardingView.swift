3b23012a7006a6367212d650f2755b3bbc42c723
Prithvi Rao Ratti <prithvimagic@gmail.com>
2022-03-05 20:07:02 +0530

    Onboarding basic UI developed

--- RestartApp/Screens/OnboardingView.swift
+++ RestartApp/Screens/OnboardingView.swift
@@ -48,6 +48,47 @@ struct OnboardingView: View {
                     
                 }//: ZSTACK
                 Spacer()
+                
+                ZStack{
+                    Capsule()
+                        .fill(Color.white.opacity(0.2))
+                    Capsule()
+                        .fill(Color.white.opacity(0.2))
+                        .padding(8)
+                    HStack{
+                        Capsule()
+                            .fill(Color("ColorRed"))
+                            .frame(width: 80)
+                        Spacer()
+                    }//: HSTACK
+                    
+                    Text("Get Started")
+                        .font(.system(.title3, design: .rounded))
+                        .fontWeight(.bold)
+                        .foregroundColor(.white)
+                        .offset(x: 20)
+                    
+                    HStack{
+                        ZStack{
+                            Circle()
+                                .fill(Color("ColorRed"))
+                            Circle()
+                                .fill(Color.black.opacity(0.15))
+                                .padding(8)
+                            Image(systemName: "chevron.right.2")
+                                .font(.system(size: 24, weight: .bold))
+                        }//: ZSTACK
+                        .foregroundColor(.white)
+                        .frame(width: 80, height: 80, alignment: .center)
+                        .onTapGesture {
+                            isOnboardActive = false
+                        }
+                        Spacer()
+                    }//: HSTACK
+                    
+                }//: ZSTACK
+                .frame(height: 80, alignment: .center)
+                .padding()
             }//: VSTACK
         }//: ZSTACK
     }
